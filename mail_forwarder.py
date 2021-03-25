df58262bd469a738002d1a34e37deb0fe5fd0c3c
stonelin <jason840708@gmail.com>
2021-03-25 17:36:26 +0800

    Exception handling

--- mail_forwarder.py
+++ mail_forwarder.py
@@ -2,7 +2,10 @@ from smtplib import SMTP
 from imap_tools import MailBox, UidRange, AND
 import email
 import logging
+import datetime as dt
 from email import policy
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from logging.handlers import TimedRotatingFileHandler
 
 import json
@@ -15,7 +18,9 @@ class MailForwarder:
     def __init__(self, config, last_send, members, subject_filter, logger):
         self.config = json.load(open(config))
         self.last_send = last_send
-        self.members = json.load(open(members))
+        m = json.load(open(members))
+        self.users = m['users']
+        self.managers = m['managers']
         self.logger = logger
         self.subject_filter = [x.strip()
                                for x in open(subject_filter).readlines()]
@@ -31,18 +36,50 @@ class MailForwarder:
     def _set_last_send(self, value):
         open(self.last_send, 'w').write(str(value))
 
-    def _get_member_emails(self):
-        return self.members.values()
+    def _get_user_emails(self):
+        return self.users.values()
+
+    def _get_manager_emails(self):
+        return self.users.values()
+
+    def _create_error_mail(self, subject, error_text):
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = subject
+        msg['From'] = self.config['smtpuser']
+        msg['To'] = ','.join(self._get_manager_emails())
+        msg.attach(MIMEText(error_text, 'plain'))
+        return msg
+
+    def _send_mails_to_manager(self, mail):
+        try:
+            with SMTP(self.config['host'], port=self.config['smtp_port']) as server:
+                server.starttls()
+                l = server.login(
+                    self.config['smtp_user'], self.config['smtp_pw'])
+                ret = server.sendmail(self.config['smtp_user'],
+                                      self._get_manager_emails(),
+                                      mail.as_string())
+                self.logger.error(f'`{mail["subject"]}` sending failed')
+                for address, err in ret.items():
+                    self.logger.error(f'{address}: {err}')
+        except Exception as e:
+            self.logger.error(f'`{mail["subject"]}` sending failed : {e}')
 
     def update(self):
         # at least return the mail with 'last_send' uid
         # if there is new mail, there will be more than
         # one uid
         query = AND(uid=UidRange(self._get_last_send(), '*'))
+        mails = None
+        try:
+            with MailBox(self.config['host'], port=self.config['imap_port'], starttls=True).login(self.config['imap_user'], self.config['imap_pw'], 'INBOX') as mailbox:
+                mails = list(map(lambda msg: (msg.uid, email.message_from_bytes(msg.obj.as_bytes(), policy=policy.default)),
+                                 mailbox.fetch(query, mark_seen=False)))
+        except Exception as e:
+            self.logger.error(f'fetch mail failed : {e}')
 
-        with MailBox(self.config['host'], port=self.config['imap_port'], starttls=True).login(self.config['imap_user'], self.config['imap_pw'], 'INBOX') as mailbox:
-            mails = list(map(lambda msg: (msg.uid, email.message_from_bytes(msg.obj.as_bytes(), policy=policy.default)),
-                             mailbox.fetch(query, mark_seen=False)))
+        if mails is None:
+            return
 
         if len(mails) > 1:
             # ignore the first email which has been sent last time
@@ -55,27 +92,38 @@ class MailForwarder:
             time.sleep(interval)
 
     def _forward_email(self, mails):
-        with SMTP(self.config['host'], port=self.config['smtp_port']) as server:
-            server.starttls()
-            l = server.login(self.config['smtp_user'], self.config['smtp_pw'])
-            for uid, mail in mails:
-                subject = mail["Subject"]
-                if self._is_filtered(mail):
-                    self.logger.info(f'{uid}: `{subject}` has been filtered')
-                else:
-                    del mail['To']
-                    mail['To'] = ','.join(self._get_member_emails())
-                    ret = server.sendmail(self.config['smtp_user'],
-                                          self._get_member_emails(),
-                                          mail.as_string())
-                    if len(ret) > 0:
-                        self.logger.error(f'{uid}: `{subject}` sending failed')
-                        for address, err in ret.items():
-                            self.logger.error(f'{address}: {err}')
+        try:
+            with SMTP(self.config['host'], port=self.config['smtp_port']) as server:
+                server.starttls()
+                l = server.login(
+                    self.config['smtp_user'], self.config['smtp_pw'])
+                for uid, mail in mails:
+                    subject = mail["Subject"]
+                    if self._is_filtered(mail):
+                        self.logger.info(
+                            f'{uid}: `{subject}` has been filtered')
                     else:
-                        self.logger.info(f'{uid}: `{subject}` has been sent')
+                        del mail['To']
+                        mail['To'] = ','.join(self._get_user_emails())
+                        ret = server.sendmail(self.config['smtp_user'],
+                                              self._get_user_emails(),
+                                              mail.as_string())
 
-                    time.sleep(self.config['send_interval'])
+                        if len(ret) > 0:
+                            self.logger.error(
+                                f'{uid}: `{subject}` sending failed')
+                            for address, err in ret.items():
+                                self.logger.error(f'{address}: {err}')
+                        else:
+                            self.logger.info(
+                                f'{uid}: `{subject}` has been sent')
+
+                        time.sleep(self.config['send_interval'])
+        except Exception as e:
+            self.logger.error(f'sending mail failed : {e}')
+            self._send_mails_to_manager(self._create_error_mail(
+                f'{dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} : Sending Mail Failed',
+                str(e)))
 
 
 def create_logger(logname, console=False):
@@ -105,6 +153,8 @@ def create_logger(logname, console=False):
 
 def main():
     logger = create_logger('log/forward.log')
+    logger.info('Start')
+
     forwarder = MailForwarder(
         config='config.json',
         last_send='last_send.txt',
